e786378acf60268c1fa8f58c890135838732c1a4
Lauren Stone <lstone321@gmail.com>
2020-07-17 14:07:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,6 @@ language = ['ruby', 'javascript', 'python', 'objective-c']
 def my_collect(language)
   new_lang = []
 language.each do |language|
-  while i < language.length
     yield language[i]
     i += 1 
   end
