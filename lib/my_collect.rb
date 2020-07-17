afa01adec8a1362f8a112f998922d24daa59679d
Lauren Stone <lstone321@gmail.com>
2020-07-17 14:00:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+language = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(language)
 
+my_collect(language) do |language|
+  while language += 1 
+    yield 
+  end
+  language.upcase
+  end
+end
+    
+  
